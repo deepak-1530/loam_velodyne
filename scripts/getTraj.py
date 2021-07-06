ebee267129db00f84f6e0488ae61c90d43094c4b
Deepak singh <deepaks.ec.16@nsit.net.in>
2021-07-06 16:51:51 +0530

    added poses.npy for pose data from loam

--- scripts/getTraj.py
+++ scripts/getTraj.py
@@ -1,27 +1,95 @@
 # subscribe to odometry topic /integrated_to_init
 
+import math
 import rospy
+import numpy as np
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import Path
+from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 
 trajectory =  Path()
 trajPub = rospy.Publisher("/slamTrajectory", Path, queue_size=1)
+markerPub = rospy.Publisher("/slamTrajMarker", MarkerArray, queue_size=1)
+
+
+count = 0
+timeStamp = 0
+poses = []
+
+def euler_from_quaternion(x, y, z, w):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)*(180.0/3.14)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)*(180.0/3.14)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)*(180.0/3.14)
+     
+        return roll_x, pitch_y, yaw_z # in radians
+
+def pointCloudCb(msg):
+    global timeStamp
+    #print(msg.header)
+    timeStamp = str(msg.header.stamp.secs)  + "." + str(msg.header.stamp.nsecs)
 
 def odomCb(msg):
-    print(msg.pose.pose.position)
+    global count
+    global timeStamp
     p = PoseStamped()
+    m = MarkerArray()
+    marker = Marker()
+
     trajectory.header.frame_id = msg.header.frame_id
     trajectory.header.stamp = rospy.Time.now()
     p.pose.position = msg.pose.pose.position
     p.pose.orientation = msg.pose.pose.orientation
-    print(p)
+    
+    marker.header.frame_id = msg.header.frame_id
+    marker.type = marker.SPHERE
+    marker.action = marker.ADD
+    marker.scale.x = 0.7
+    marker.scale.y = 0.7
+    marker.scale.z = 0.7
+    marker.color.a = 1.0
+    marker.color.r = 1.0
+    marker.color.g = 0
+    marker.color.b = 0
+    marker.pose.orientation.w = 1.0
+    marker.pose.position = p.pose.position
+    marker.id = count
+    marker.ns = str(count)
+    m.markers.append(marker)
+
     trajectory.poses.append(p)
     trajPub.publish(trajectory)
+    markerPub.publish(m)
+    roll, pitch, yaw = euler_from_quaternion(p.pose.orientation.x, p.pose.orientation.y, p.pose.orientation.z, p.pose.orientation.w)
+    x = [float(timeStamp), p.pose.position.x, p.pose.position.y, p.pose.position.z, roll, pitch, yaw]
+    poses.append(x)
+    posesNp = np.asanyarray(poses)
+    print(x)
+    print("\n")
+    np.save("poses.npy", posesNp)
+    count += 1
     
 if __name__=="__main__":
     rospy.init_node("trajectory_node")
     print("Here")
     odomSub = rospy.Subscriber("/integrated_to_init", Odometry, odomCb)
-    rospy.spin()    
+    pointCloudSub = rospy.Subscriber("/velodyne_points", PointCloud2, pointCloudCb)
+    rospy.spin()
 
